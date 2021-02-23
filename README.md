aa678a38be435bd55ea22e16501773934b78e5df
Srikanth Machiraju <vism@microsoft.com>
2021-02-24 00:30:29 +0530

    added email and price range use cases

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
-##  
+#  Rasa Chat Bot
 
 
-## Setup
+## Environment Setup
 
 
-`
+```
 conda create rasa
 conda activate rasa
-
 pip install rasa 
 pip install rasa[Spacy]
-
 pip install pandas
+```
 
+## Dev and Test
 
+```
 rasa train
-
 rasa run actions
-
 rasa shell
-`
+```
--- actions.py
+++ actions.py
@@ -6,15 +6,25 @@ from rasa_sdk import Action
 from rasa_sdk.events import SlotSet
 import pandas as pd
 import json
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 ZomatoData = pd.read_csv('zomato.csv')
 ZomatoData = ZomatoData.drop_duplicates().reset_index(drop=True)
 
-def RestaurantSearch(City,Cuisine):
-	TEMP = ZomatoData[(ZomatoData['Cuisines'].apply(lambda x: Cuisine.lower() in x.lower())) & (ZomatoData['City'].apply(lambda x: City.lower() in x.lower()))]
+def RestaurantSearch(City,Cuisine,Price):
+	if(Price == "low"):
+		TEMP = ZomatoData[(ZomatoData['Cuisines'].apply(lambda x: Cuisine.lower() in x.lower())) \
+		& (ZomatoData['City'].apply(lambda x: City.lower() in x.lower())) & \
+		(ZomatoData["Average Cost for two"] <= 300)]
+	elif(Price == "mid"):
+		TEMP = ZomatoData[(ZomatoData['Cuisines'].apply(lambda x: Cuisine.lower() in x.lower())) \
+		& (ZomatoData['City'].apply(lambda x: City.lower() in x.lower())) & \
+		((ZomatoData["Average Cost for two"] > 300) & (ZomatoData["Average Cost for two"] <= 700))]
 	return TEMP[['Restaurant Name','Address','Average Cost for two','Aggregate rating']]
 
-class ActionSearchRestaurants(Action):
+class ActionSearchRestaurants(Action):	
 	def name(self):
 		return 'action_search_restaurants'
 
@@ -22,7 +32,7 @@ class ActionSearchRestaurants(Action):
 		loc = tracker.get_slot('location')
 		cuisine = tracker.get_slot('cuisine')
 		price = tracker.get_slot('price')
-		results = RestaurantSearch(City=loc,Cuisine=cuisine)
+		results = RestaurantSearch(City=loc,Cuisine=cuisine,Price=price)
 		response=""
 		if results.shape[0] == 0:
 			response= "Sorry, we do not operate in that area yet."
@@ -34,3 +44,25 @@ class ActionSearchRestaurants(Action):
 		dispatcher.utter_message("-----"+response)
 		return [SlotSet('location',loc)]
 
+class ActionSendEmail(Action):
+    
+    def name(self):
+        return 'action_send_email'
+
+    def run(self, dispatcher, tracker, domain):
+        from_user = 'sriks@gmail.com'
+        to_user = tracker.get_slot('email')
+        password = 'password'
+        server = smtplib.SMTP('smtp.gmail.com',587)
+        server.starttls()
+        server.login(from_user, password)
+        subject = 'Your list of restaurants from Foodie'
+        msg = MIMEMultipart()
+        msg['From'] = from_user
+        msg['TO'] = to_user
+        msg['Subject'] = subject
+        body = tracker.get_slot('emailbody')
+        msg.attach(MIMEText(body,'plain'))
+        text = msg.as_string()
+        server.sendmail(from_user,to_user,text)
+        server.close()
