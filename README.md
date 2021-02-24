f133327189bd4922acf9c7cf93806c7fd86ab703
Srikanth Machiraju <vism@microsoft.com>
2021-02-24 18:47:33 +0530

    tested email use cases

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 #  Rasa Chat Bot
 
+## Version Information
+
+```
+Rasa Version     : 2.3.0
+Rasa SDK Version : 2.3.1
+Rasa X Version   : None
+Python Version   : 3.7.9
+Operating System : Windows-10-10.0.19041-SP0
+```
 
 ## Environment Setup
-
 
 ```
 conda create rasa
@@ -15,6 +23,7 @@ pip install pandas
 ## Dev and Test
 
 ```
+rasa data validate
 rasa train
 rasa run actions
 rasa shell
--- actions.py
+++ actions.py
@@ -14,15 +14,14 @@ ZomatoData = pd.read_csv('zomato.csv')
 ZomatoData = ZomatoData.drop_duplicates().reset_index(drop=True)
 
 def RestaurantSearch(City,Cuisine,Price):
+	zomato_data = ZomatoData[(ZomatoData['Cuisines'].apply(lambda x: Cuisine.lower() in x.lower())) & (ZomatoData['City'].apply(lambda x: City.lower() in x.lower()))]
 	if(Price == "low"):
-		TEMP = ZomatoData[(ZomatoData['Cuisines'].apply(lambda x: Cuisine.lower() in x.lower())) \
-		& (ZomatoData['City'].apply(lambda x: City.lower() in x.lower())) & \
-		(ZomatoData["Average Cost for two"] <= 300)]
+		TEMP = zomato_data[ZomatoData["Average Cost for two"] <= 300]
 	elif(Price == "mid"):
-		TEMP = ZomatoData[(ZomatoData['Cuisines'].apply(lambda x: Cuisine.lower() in x.lower())) \
-		& (ZomatoData['City'].apply(lambda x: City.lower() in x.lower())) & \
-		((ZomatoData["Average Cost for two"] > 300) & (ZomatoData["Average Cost for two"] <= 700))]
-	return TEMP[['Restaurant Name','Address','Average Cost for two','Aggregate rating']]
+		TEMP = zomato_data[(ZomatoData["Average Cost for two"] > 300) & (ZomatoData["Average Cost for two"] <= 700)]
+	elif(Price == "high"):
+		TEMP = zomato_data[(ZomatoData["Average Cost for two"] > 700)]
+	return TEMP[['Restaurant Name','Address','Average Cost for two','Aggregate rating']].sort_values('Aggregate rating', ascending = False)[:10]
 
 class ActionSearchRestaurants(Action):	
 	def name(self):
@@ -34,35 +33,45 @@ class ActionSearchRestaurants(Action):
 		price = tracker.get_slot('price')
 		results = RestaurantSearch(City=loc,Cuisine=cuisine,Price=price)
 		response=""
+		count = 0
+		bot_response_header = "Showing top 5 restaurants \n===========================================\n"
+		email_response_header = "Top 10 restaurants \n===========================================\n"
 		if results.shape[0] == 0:
-			response= "Sorry, we do not operate in that area yet."
+				dispatcher.utter_message("Sorry, no restaurant found for your criteria. Would you like to search for some other restaurants?")
 		else:
-			for restaurant in RestaurantSearch(loc,cuisine).iloc[:5].iterrows():
+			for restaurant in results.iterrows():
+				count = count + 1
 				restaurant = restaurant[1]
-				response=response + F"Found {restaurant['Restaurant Name']} in {restaurant['Address']} rated {restaurant['Address']} with avg cost {restaurant['Average Cost for two']} \n\n"
-				
-		dispatcher.utter_message("-----"+response)
-		return [SlotSet('location',loc)]
+				response = response + F"Found {restaurant['Restaurant Name']} in {restaurant['Address']} has been rated {restaurant['Aggregate rating']}, average cost for two is {restaurant['Average Cost for two']}\n\n"
+				if (count == 5):
+					dispatcher.utter_message(bot_response_header + response) # dispatch the message to caller
+			if(count<5 and count >0):
+					dispatcher.utter_message(bot_response_header + response)
+		print(response)
+		return [SlotSet('email_body', email_response_header + response)]
 
 class ActionSendEmail(Action):
-    
-    def name(self):
-        return 'action_send_email'
+	
+	def name(self):
+		return 'action_send_email'
 
-    def run(self, dispatcher, tracker, domain):
-        from_user = 'sriks@gmail.com'
-        to_user = tracker.get_slot('email')
-        password = 'password'
-        server = smtplib.SMTP('smtp.gmail.com',587)
-        server.starttls()
-        server.login(from_user, password)
-        subject = 'Your list of restaurants from Foodie'
-        msg = MIMEMultipart()
-        msg['From'] = from_user
-        msg['TO'] = to_user
-        msg['Subject'] = subject
-        body = tracker.get_slot('emailbody')
-        msg.attach(MIMEText(body,'plain'))
-        text = msg.as_string()
-        server.sendmail(from_user,to_user,text)
-        server.close()
+	def run(self, dispatcher, tracker, domain):
+			try:
+				from_user = 'upgrad.sriks@gmail.com' # replace email with your own
+				to_user = tracker.get_slot('email')
+				password = 'ap28be4393' # replace password with your own
+				server = smtplib.SMTP('smtp.gmail.com',587)
+				server.starttls()
+				server.login(from_user, password)
+				subject = 'Your list of restaurants from Foodie'
+				msg = MIMEMultipart()
+				msg['From'] = from_user
+				msg['TO'] = to_user
+				msg['Subject'] = subject
+				body = tracker.get_slot('email_body')
+				msg.attach(MIMEText(body,'plain'))
+				text = msg.as_string()
+				server.sendmail(from_user,to_user,text)
+				server.close()
+			except: 
+				dispatcher.utter_message("Something went wrong, we could not send you the email. Please try again later.")
